dc5900d821aa49881fe33dc943128b18ac354456
microsoftdevelop <96072013+microsoftdevelop@users.noreply.github.com>
2021-12-14 20:36:14 +0100

    js

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 let date = new Date();
 const d = new Date();
 let day = d.getDate();
-let text = document.querySelector(".text1");
-text.innerHTML = (' (' + day + '.' + (date.getMonth() + 1) + '.' + date.getFullYear() + ')');
-let text1 = document.querySelector(".text2");
-text1.innerHTML = (' (' + day + '.' + (date.getMonth() + 1) + '.' + date.getFullYear() + ')');
+let texto = document.getElementById('text1');
+texto.innerHTML = (' (' + day + '.' + (date.getMonth() + 1) + '.' + date.getFullYear() + ')');
+
+let date1 = new Date();
+const d1 = new Date();
+let day1 = d1.getDate();
+
+let texto1 = document.getElementById('text2');
+texto1.innerHTML = (' (' + day1 + '.' + (date1.getMonth() + 1) + '.' + date1.getFullYear() + ')');
