b5360afdba22ccf81b083e5a037cd74817685aeb
microsoftdevelop <96072013+microsoftdevelop@users.noreply.github.com>
2022-01-02 19:36:51 +0300

    Fin

--- main.js
+++ main.js
@@ -2,9 +2,8 @@ let date = new Date();
 const d = new Date();
 let day = d.getDate();
 let texto = document.getElementById('text1');
-var checkMonth = date.getMonth();
-
-if (checkMonth<9) {
+var check = date.getMonth();
+if (check<9) {
 texto.innerHTML = (' (' + day + '.' + '0' + (date.getMonth() + 1) + '.' + date.getFullYear() + ')');
 } else {
 texto.innerHTML = (' (' + day + '.' + (date.getMonth() + 1) + '.' + date.getFullYear() + ')');
@@ -16,7 +15,7 @@ const d1 = new Date();
 let day1 = d1.getDate();
 let texto1 = document.getElementById('text2');
 
-if (checkMonth<9) {
+if (check<9) {
 texto1.innerHTML = (' (' + day1 + '.' + '0' + (date.getMonth() + 1) + '.' + date1.getFullYear() + ')');
 } else {
 texto1.innerHTML = (' (' + day1 + '.' + (date.getMonth() + 1) + '.' + date1.getFullYear() + ')');
