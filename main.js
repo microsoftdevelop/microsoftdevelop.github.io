9f5580b12d8c44777faf14d8a0c9b636d9ea867f
microsoftdevelop <96072013+microsoftdevelop@users.noreply.github.com>
2022-03-05 21:14:22 +0100

    JS update

--- main.js
+++ main.js
@@ -1,28 +1,21 @@
-var date = new Date();
-let text = document.getElementById('text1');
-var checkMonth = date.getMonth();
-var checkDay = date.getDate()
+"use strict";
 
-if (checkMonth<9 && checkDay<10) {
-text.innerHTML = (' (' + '0' + date.getDate() + '.' + '0' + (date.getMonth() + 1) + '.' + date.getFullYear() + ')');
-} else if (checkMonth<9){
-text.innerHTML = (' (' + date.getDate() + '.' + '0' + (date.getMonth() + 1) + '.' + date.getFullYear() + ')');
-} else if (checkDay<10){
-text.innerHTML = (' (' + '0' + date.getDate() + '.' + (date.getMonth() + 1) + '.' + date.getFullYear() + ')');
-} else {
-text.innerHTML = (' (' + date.getDate() + '.' + (date.getMonth() + 1) + '.' + date.getFullYear() + ')');
+const dateHTML = document.getElementsByClassName('date');
+
+
+const addZeros = n => {
+    if (n.toString().length < 2) return '0'.concat(n);
+    return n;
 }
 
-
-
-let textEN = document.getElementById('text2');
-
-if (checkMonth<9 && checkDay<10) {
-textEN.innerHTML = (' (' + '0' + date.getDate() + '.' + '0' + (date.getMonth() + 1) + '.' + date.getFullYear() + ')');
-} else if (checkMonth<9){
-textEN.innerHTML = (' (' + date.getDate() + '.' + '0' + (date.getMonth() + 1) + '.' + date.getFullYear() + ')');
-} else if (checkDay<10){
-textEN.innerHTML = (' (' + '0' + date.getDate() + '.' + (date.getMonth() + 1) + '.' + date.getFullYear() + ')');
-} else {
-textEN.innerHTML = (' (' + date.getDate() + '.' + (date.getMonth() + 1) + '.' + date.getFullYear() + ')');
+const changeDate = () => {
+    const date = new Date();
+    let day = addZeros(date.getDate());
+    let month = addZeros(date.getMonth() + 1);
+    let year = date.getFullYear();
+    for (let i = 0; i < dateHTML.length; i++) {
+        dateHTML[i].innerHTML = (`(${day}.${month}.${year})`)
+    }
 }
+
+changeDate();
