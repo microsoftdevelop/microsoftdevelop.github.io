6fec035b1d8acf938a31ab5e475229b3a51b83f1
microsoftdevelop <96072013+microsoftdevelop@users.noreply.github.com>
2022-03-23 18:11:18 +0100

    Final result with js code adjustment

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
-let date = new Date();
-const d = new Date();
-let day = d.getDate();
-let texto = document.getElementById('text1');
-texto.innerHTML = (' (' + day + '.' + (date.getMonth() + 1) + '.' + date.getFullYear() + ')');
+"use strict";
 
-let date1 = new Date();
-const d1 = new Date();
-let day1 = d1.getDate();
+const dateHTML = document.getElementsByClassName('date');
 
-let texto1 = document.getElementById('text2');
-texto1.innerHTML = (' (' + day1 + '.' + (date1.getMonth() + 1) + '.' + date1.getFullYear() + ')');
+
+const addZeros = n => {
+    if (n.toString().length < 2) return '0'.concat(n);
+    return n;
+}
+
+const changeDate = () => {
+    const date = new Date();
+    let day = addZeros(date.getDate());
+    let month = addZeros(date.getMonth() + 1);
+    let year = date.getFullYear();
+    for (let i = 0; i < dateHTML.length; i++) {
+        dateHTML[i].innerHTML = (`(${day}.${month}.${year})`)
+    }
+}
+
+changeDate();
